26dc4c705a9519464db466972fee76231f8460b4
Tristan Sun <tsun2019@gmail.com>
2017-11-30 15:23:36 -0500

    Added code to show hidden house card

--- src/App.js
+++ src/App.js
@@ -8,7 +8,7 @@ class App extends React.Component {
   constructor(props){
     super(props);
     this.state = {
-      deck: this.shuffle([
+      deck: [...this.shuffle([
         {v:11,f:"c1"},{v:2,f:"c2"},{v:3,f:"c3"},{v:4,f:"c4"},{v:5,f:"c5"},{v:6,f:"c6"},
         {v:7,f:"c7"},{v:8,f:"c8"},{v:9,f:"c9"},{v:10,f:"c10"},{v:10,f:"c11"},{v:10,f:"c12"},{v:10,f:"c13"},
         {v:11,f:"h1"},{v:2,f:"h2"},{v:3,f:"h3"},{v:4,f:"h4"},{v:5,f:"h5"},{v:6,f:"h6"},
@@ -17,7 +17,7 @@ class App extends React.Component {
         {v:7,f:"s7"},{v:8,f:"s8"},{v:9,f:"s9"},{v:10,f:"s10"},{v:10,f:"s11"},{v:10,f:"s12"},{v:10,f:"s13"},
         {v:11,f:"d1"},{v:2,f:"d2"},{v:3,f:"d3"},{v:4,f:"d4"},{v:5,f:"d5"},{v:6,f:"d6"},
         {v:7,f:"d7"},{v:8,f:"d8"},{v:9,f:"d9"},{v:10,f:"d10"},{v:10,f:"d11"},{v:10,f:"d12"},{v:10,f:"d13"}
-        ])
+        ])]
     }; 
   }
 
@@ -51,6 +51,7 @@ class Game extends React.Component {
 
   //create functions to handle certain events (dealing,hit,stand,restart,advice)
 
+  //this function signature is special bc it saves the "this" scope.
   handleDeal = () => {
     const newDeck = [...this.state.deck];
     const newPlayerHand = [...this.state.playerHand];
@@ -61,6 +62,7 @@ class Game extends React.Component {
     newHouseHand.push(newDeck.pop());
     newPlayerHand.push(newDeck.pop());
     //need to push a card and hide it in dealerHand bc 2nd card is hidden. 
+    newHouseHand.push(newDeck.pop());
 
     //make updates
     this.setState({
@@ -72,7 +74,7 @@ class Game extends React.Component {
   }
 
   
-  handleHit() {
+  handleHit = () => {
     const deck = this.state.deck;
     var playerHand = this.state.playerHand;
     
@@ -99,11 +101,12 @@ class Game extends React.Component {
     return (
       //render a status here
       <div>
-        <Hand hand={this.state.houseHand}/>
+        {this.state.status} 
+        <Hand hand={this.state.houseHand} dealer={true}/>
         <Interface
           deal={this.handleDeal}
         />
-        <Hand hand ={this.state.playerHand}/>
+        <Hand hand ={this.state.playerHand} dealer={false}/>
       </div>
     );
   }
@@ -126,24 +129,30 @@ class Hand extends React.Component {
   //   })
   // }
   
-  // getSum(hand){
-  //   var sum = 0;
-  //   for(var i = 0; i < hand.length; i++){
-  //     sum += hand[i].value;
-  //   }
-  //   return sum;
-  // }
+  //function to hold
+  getSum(hand){
+    var sum = 0;
+    for(var i = 0; i < hand.length; i++){
+      sum += hand[i].v;
+    }
+    return sum;
+  }
 
   
 
   render() {
     return (
       <div>
-        {this.props.hand.map(function(card,i){
-          return <Card value={card.v} face={card.f} key={card.f}/>
+        { //if dealer map, else map 
+          this.props.hand.map((card,i) => {
+          //if its part of the houseHand, then pass a hidden true part. 
+          if (this.props.dealer && i === 1) 
+            return <Card value={card.v} face={card.f} key={card.f} hidden={true}/>
+          else
+            return <Card value={card.v} face={card.f} key={card.f} hidden={false}/>
         })}
         <div>
-          {/* Hand score: {this.getSum(this.state.hand)}  */}
+          Hand score: {this.getSum(this.props.hand)} 
         </div>
       </div>
     );
@@ -160,7 +169,11 @@ class Card extends React.Component {
     :*/
     const cardImage = this.props.value + '_of_' + this.props.face;
     var url = require('./svg-cards/' + cardImage + '.svg');
-    var cardBackground =  'url(' + url + ')';
+    var urlHidden = require('./svg-cards/hidden.png');
+    var cardBackground =
+      (this.props.hidden) 
+      ? 'url(' + urlHidden + ')'
+      : 'url(' + url + ')';
     var card = {backgroundImage: cardBackground};
     return (
       <div className='card' style={card}/>
