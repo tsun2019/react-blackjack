b49fe7d7de13d6693157e8dd073d8f85d05f9370
Tristan Sun <tsun2019@gmail.com>
2017-12-08 18:35:22 -0500

    Changed card images to something smaller to fit better in my game format. Now you don't have to scroll at all to see the whole game... Also, it is perfectly horizontally responsive now on any small or larger screen. On extra small screens(smart phones), the instructions/rules will stack on top and the game will be moved below that. Users would have to scroll down to see it, but they can still see/play the game's whole interface without scrolling once they scroll below the instructions/rules. Note: still need to work on vertical responsiveness...

--- src/App.js
+++ src/App.js
@@ -9,7 +9,7 @@ class App extends React.Component {
   constructor(props){
     super(props);
     this.state = {
-      deck: [...this.shuffle(this.shuffle([
+      deck: [...this.shuffle(this.shuffle(this.shuffle([
         {v:11,f:"c1"},{v:2,f:"c2"},{v:3,f:"c3"},{v:4,f:"c4"},{v:5,f:"c5"},{v:6,f:"c6"},
         {v:7,f:"c7"},{v:8,f:"c8"},{v:9,f:"c9"},{v:10,f:"c10"},{v:10,f:"c11"},{v:10,f:"c12"},{v:10,f:"c13"},
         {v:11,f:"h1"},{v:2,f:"h2"},{v:3,f:"h3"},{v:4,f:"h4"},{v:5,f:"h5"},{v:6,f:"h6"},
@@ -74,7 +74,7 @@ class App extends React.Component {
         {v:7,f:"s7"},{v:8,f:"s8"},{v:9,f:"s9"},{v:10,f:"s10"},{v:10,f:"s11"},{v:10,f:"s12"},{v:10,f:"s13"},
         {v:11,f:"d1"},{v:2,f:"d2"},{v:3,f:"d3"},{v:4,f:"d4"},{v:5,f:"d5"},{v:6,f:"d6"},
         {v:7,f:"d7"},{v:8,f:"d8"},{v:9,f:"d9"},{v:10,f:"d10"},{v:10,f:"d11"},{v:10,f:"d12"},{v:10,f:"d13"},
-        ]))],
+        ])))],
     }; 
   }
 
@@ -565,7 +565,7 @@ class Interface extends React.Component {
   render() {
     return (
       <div>
-        <h3>
+        <h3 class ="houseScore">
           House Hand Score: {this.props.housescore}
         </h3>
       <div>
@@ -578,13 +578,15 @@ class Interface extends React.Component {
        <button onClick={this.props.restart} type="button">Next Round</button>
        
       </div>
-      <h3>
-          Player Hand Score: {this.props.playerscore}
-      </h3>
+      
+      <Result status={this.props.status}/>
+      
+        
       <div class="playerInfo">
-        
-        <Result status={this.props.status}/>
-        
+        <h3 class="playerScore">
+            Player Hand Score: {this.props.playerscore}
+        </h3>
+      
         <div>
           Player Wins: {this.props.tally}
         </div>
